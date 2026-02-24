ff130f121c4e6c471f34e8b23d6edbbc2d541353
Juan Perez <miltonmontiel951@gmail.com>
2026-02-24 20:27:41 -0300

    Finalizando backend y validaciones del proyecto

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import connectDB from './config/database.js';
-const PORT = process.env.PORT || 3000;
-// Conectar a la base de datos
-connectDB();
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
